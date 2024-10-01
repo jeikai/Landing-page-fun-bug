1ff263c0723a8908699abcd0a7f7b78ff18657a1
jeikai <98737616+jeikai@users.noreply.github.com>
2024-10-02 01:10:13 +0700

    Revert "change 3d to 2d image"
    This reverts commit cf1f93a770323299b0ecde4f3d444d0a78c6a6b9.

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,23 +1,19 @@
 import React from "react";
+
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
 const Tech = () => {
   return (
     <div className='flex flex-row flex-wrap justify-center gap-10'>
       {technologies.map((technology) => (
-        <div 
-          className='w-28 h-28 bg-white rounded-full flex items-center justify-center p-2' 
-          key={technology.name}
-        >
-          <img 
-            src={technology.icon} 
-            alt={technology.name} 
-            className="w-full h-full object-contain rounded-full" 
-          />
+        <div className='w-28 h-28' key={technology.name}>
+          <BallCanvas icon={technology.icon} />
         </div>
       ))}
     </div>
   );
 };
 
-export default Tech;
+export default SectionWrapper(Tech, "");
